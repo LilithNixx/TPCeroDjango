d3e27aa858ffe911e3095470de835c6ac34a15be
Daniela Sol <andromeda0209@gmail.com>
2025-04-11 00:17:36 -0300

    se creo el template detail.html con un formulario

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
 #rom django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from urllib import response
 from django.http import HttpResponse
 from . models import Question
-from django.http import Http404
+#from django.http import Http404
 
 
 #def index(request):
@@ -17,12 +17,16 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
-
+'''
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.NoesNotExist:
         raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+'''
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
