a8932bf102d373e10aaaee396d5f7bf2ecc3baac
Daniela Sol <andromeda0209@gmail.com>
2025-04-13 19:22:34 -0300

    pasar todo a generic views

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from . models import Question, Choice
 from django.db.models import F # Permite hacer operaciones directas en la base de datos (como votos + 1)
 from django.urls import reverse
+from django.views import generic
 #from django.http import Http404
 
 
@@ -14,11 +15,20 @@ from django.urls import reverse
     #context = {"latest_question_list": latest_question_list}
     #return HttpResponse(template.render(context, request))
 
+'''
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
-
+'''
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_objext_name = "latest_question_list"
+    
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+        #return the last 5 published questions
+    
 '''
 def detail(request, question_id):
     try:
@@ -26,11 +36,17 @@ def detail(request, question_id):
     except Question.NoesNotExist:
         raise Http404("Question does not exist")
     return render(request, "polls/detail.html", {"question": question})
-'''
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
+'''
 
+class DetailView(generic.DetailView):
+    model = Questiontemplate_name = "polls/detail.html"
+    
+
+'''    
 #muestra los resultados de una pregunta específica, incluyendo probablemente cuántos votos tiene cada opción (Choice).
 def results(request, question_id):
     #Intenta obtener la instancia de Question con ID igual a question_id.
@@ -40,12 +56,19 @@ def results(request, question_id):
     #Renderiza la plantilla HTML polls/result.html.
     #Le pasa el contexto {"question": question}.
     return render(request, "polls/result.html", {"question": question})
+'''
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    
+    
 '''
 cuando usás render(request, "plantilla.html", contexto), ese tercer parámetro (contexto) es un diccionario de Python donde:
 la clave ("question") es el nombre con el que vas a acceder en la plantilla.
 el valor (question) es la variable de Python que contiene los datos (en este caso, una instancia del modelo Question).
 '''
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -63,4 +86,6 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
 
