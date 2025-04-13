c42f053dc91cd385ff19432d9a50a7e563b65223
Daniela Sol <andromeda0209@gmail.com>
2025-04-13 15:05:06 -0300

    implementacion de la votacion usando get_object_or_404 y try-escept

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
-#rom django.template import loader
+#from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from urllib import response
-from django.http import HttpResponse
-from . models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from . models import Question, Choice
+from django.db.models import F # Permite hacer operaciones directas en la base de datos (como votos + 1)
+from django.urls import reverse
 #from django.http import Http404
 
 
@@ -33,5 +35,20 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "No elegiste una opción"
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
